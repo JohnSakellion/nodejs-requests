5c059f2bd350a22e24e5727a69f34968f247b810
Johnny Sakellion <sakellion.john@gmail.com>
2019-12-03 22:06:03 +0200

    3 reqs done

---
+++ index.js
@@ -0,0 +1,34 @@
+const rp = require('request-promise');
+const baseUri = 'http://code-school-workshop.ir-e1.cloudhub.io/api/';
+
+
+const postRequestBody = {
+    name: "Lord",
+    surname: "Johnny",
+    email: "john@thelord.com",
+    team: "British row",
+    project: "Tea and Crumpets"
+};
+
+const usersPostOptions = {
+    method: 'POST',
+    uri: baseUri+'user',
+    body: postRequestBody,
+    json: true
+};
+
+
+//GET all users
+
+//rp(baseUri+'user').then((users) => console.log(JSON.parse(users))).catch(err => console.log(err));
+
+//POST a user
+
+//rp(usersPostOptions).then((postedUser) => console.log(postedUser)).catch(err => console.log(err));
+
+rp(baseUri+'user?_id=5de4ed1372042670ff076424').then(foundUser => console.log(JSON.parse(foundUser))).catch(err => console.log(err));
+
+
+/*
+//todo /user, get, post, /user/{id} get, put, delete
+*/
