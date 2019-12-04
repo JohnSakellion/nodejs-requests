2f54857467bda57a8b3ae5eee4523824f1fb8ddc
Johnny Sakellion <sakellion.john@gmail.com>
2019-12-04 22:14:57 +0200

    Made the code better looking

--- index.js
+++ index.js
@@ -34,23 +34,30 @@ const updateUserOptions = {
 
 //GET all users
 
-//rp(baseUri+'user').then((users) => console.log(JSON.parse(users))).catch(err => console.log(err));
+rp(baseUri+'user')
+    .then((users) => console.log(JSON.parse(users)))
+    .catch(err => console.log(err));
 
 //POST a user
 
-//rp(usersPostOptions).then((postedUser) => console.log(postedUser)).catch(err => console.log(err));
+rp(usersPostOptions)
+    .then((postedUser) => console.log(postedUser))
+    .catch(err => console.log(err));
 
 //GET user by id
 
-//rp(baseUri+'user/5de4ed1372042670ff076424').then(foundUser => console.log(JSON.parse(foundUser))).catch(err => console.log(err));
+rp(baseUri+'user/5de4ed1372042670ff076424')
+    .then(foundUser => console.log(JSON.parse(foundUser)))
+    .catch(err => console.log(err));
 
 //DELETE user by id
 
-//rp(userDeleteOptions).then(deletedUser => console.log(deletedUser)).catch(err => console.log(err));
+rp(userDeleteOptions)
+    .then(deletedUser => console.log(deletedUser))
+    .catch(err => console.log(err));
 
 //UPDATE (put) user by id
 
-rp(updateUserOptions).then(updatedUser => console.log(updatedUser)).catch(err => console.log(err));
-/*
-//todo /user, get, post, /user/{id} get, put, delete
-*/
+rp(updateUserOptions)
+    .then(updatedUser => console.log(updatedUser))
+    .catch(err => console.log('error'));
